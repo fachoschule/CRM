31478f92c5a2e3f05d7dbf77ec309d5bbd3177e1
avazbabayev <avazbabayev@gmail.com>
2017-11-27 23:52:02 +0100

    Idea

--- n-crm/server.js
+++ n-crm/server.js
@@ -13,6 +13,5 @@ var path = __dirname + '/views/';
 var routes = require('./server/routes')(app);
 //var productroutes = require('./server/Product-Server')(app);
 var configDB = require('./server/config');
-
 var port = process.env.PORT || 8080;
 http.listen(port);
