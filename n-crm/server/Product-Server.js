c667e146b377b10d5c3ebd766860e82832a8a5df
Mai Thi <maihathi.92@gmail.com>
2017-11-29 20:53:51 +0100

    add view for product

--- n-crm/server/Product-Server.js
+++ n-crm/server/Product-Server.js
@@ -1,24 +1,4 @@
 let Product = require('../model/Product');
 
 module.exports = function(app) {
-    // Load Home page
-    app.get('/create-product',function(req,res){
-        res.render('add_product');
-    });
-    app.post('/testAddDB', function (req,res) {
-        let product = new Product();
-        product.product_id = "PR002";
-        product.product_name = "Coca cola";
-        product.image = "D://coca-cocla.png";
-        product.description = "Sugar water";
-
-        product.save(function (err) {
-            if (err) {
-                console.log(err);
-                return;
-            } else {
-                // req.flash('success', 'Product Add Success');
-            }
-        });
-    })
 }
