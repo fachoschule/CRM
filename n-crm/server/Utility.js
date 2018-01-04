59452bdbdd23435b17893c6ed2a2bf5db6517bfc
Mai Thi <maihathi.92@gmail.com>
2018-01-04 09:15:15 +0100

    fix loading supplier in product, fix error changing status of external codes after finishing add new product, fix error in created date in purchase order

--- n-crm/server/Utility.js
+++ n-crm/server/Utility.js
@@ -1,2 +1,19 @@
+var request = require('request');
+
 module.exports = {
+    converter:function (from,to,number,cb) {
+        request('https://api.fixer.io/latest?base='+from,function (error, response, body) {
+            var info = JSON.parse(body);
+            // console.log(info.rates[to]);
+            var sum =Number(info.rates[to])*number;
+            console.log(sum);
+            cb(sum);
+        })
+
+        /*
+        utility.converter('EUR','USD',200,function (data) {
+        partofJson = data;
+        console.log(partofJson);
+    })*/
+    }
 }
