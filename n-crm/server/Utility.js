5096d2e90b6cd7e21e20758d6336469f5f5f1c86
avazbabayev <avazbabayev@gmail.com>
2018-01-06 17:01:16 +0100

    fix error design pattern

--- n-crm/server/Utility.js
+++ n-crm/server/Utility.js
@@ -1,6 +1,5 @@
 var request = require('request');
 
-<<<<<<< HEAD
 var converter = function (from, to, number, cb) {
 
     request('https://api.fixer.io/latest?base=' + from, function (error, response, body) {
@@ -9,24 +8,13 @@ var converter = function (from, to, number, cb) {
         var sum = Number(info.rates[to]) * number;
         cb(sum);
     })};
-
 var decoratedConverter = function (from, to, cb) {
-        request('https://api.fixer.io/latest?base=' + from, function (error, response, body) {
-            var info = JSON.parse(body);
-            var sum = Number(info.rates[to]);
-=======
-module.exports = {
-    converter:function (from,to,cb) {
 
-        request('https://api.fixer.io/latest?base=' + from,function (error, response, body) {
-            var info = JSON.parse(body);
-            // console.log(info.rates[to]);
-            var sum =Number(info.rates[to]);
-           // console.log(sum);
->>>>>>> 9790392c60636ed2941c88ce9e3e2f886c35a920
-            cb(sum);
-        })};
-
+    request('https://api.fixer.io/latest?base=' + from, function (error, response, body) {
+        var info = JSON.parse(body);
+        var sum = Number(info.rates[to]);
+        cb(sum);
+    })};
 
         module.exports = {converter,decoratedConverter
 
