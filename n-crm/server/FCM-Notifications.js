44baf202be579650a9f4bfc3fab27186af1380bc
Mai Thi <maihathi.92@gmail.com>
2018-01-08 15:23:38 +0100

    fix bug from product

--- n-crm/server/FCM-Notifications.js
+++ n-crm/server/FCM-Notifications.js
@@ -16,54 +16,5 @@ module.exports = function(app) {
         res.render('firebase-clientview',{session:sess});
 
     });
-
-
-    //get access token
-    /*function getAccessToken() {
-        return new Promise(function(resolve, reject) {
-            var key = require('./service-account.json');
-            var jwtClient = new google.auth.JWT(
-                key.client_email,
-                null,
-                key.private_key,
-                SCOPES,
-                null
-            );
-            jwtClient.authorize(function(err, tokens) {
-                if (er
-                r) {
-                    reject(err);
-                    return;
-                }
-                resolve(tokens.access_token);
-            });
-        });
-    }
-//http request
-    var json = {
-        "message":{
-            "token" : "e7nfxKa0fck:APA91bEawrneFWSn7G6y75tjNz2LuasdyyDOs7mcctfc_qH75n-rQvw9m6NHO4kgVL_blIB1Wpqkj7Xib_zDyvvjw2YlqdmomKlP2nJhkXCnZ00Hc_iirhFt0LNZko8TeQjg-wIBiS4K",
-            "notification" : {
-                "body" : "This is an FCM notification message! ESAM",
-                "title" : "FCM Message",
-            }
-        }
-    };
-
-    var options = {
-        url: 'https://fcm.googleapis.com/v1/projects/ecrm-fh-kiel/messages:send HTTP/1.1',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization:': 'Bearer AAAAKiLc4Xo:APA91bHKImUbo3yOw-HaRwbZCK_1VC03uJsLppHi4K2GIADRI5sC74hjxb826ggjPGwGEFJHeQhwV3Mh2KyRGwlLV66AJHSda2OjRXbncWS-OoBJuestsfIK7Nd-Oz4GcHfeczofy7jh'
-        },
-        json: json
-    };
-
-    request(options, function(err, res, body) {
-        if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-            console.log(body);
-        }
-    });
-*/
+    
 };
